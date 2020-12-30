957004620ac1bf9c0f14c4c900be1d2192aac79a
Ignacio Gamboa <ign.gga@outlook.cl>
2020-12-30 13:56:42 -0300

    Modificaciones basica de la pagina, se editaron icono y contenido de la pagina.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="container">
+    <div id="pageheader">
+      <h1>Upcoming Events</h1>
+    </div>
+     <div id="bodycontent">
+      <div id="upcoming">
+        <h2>August 4</h2>
+        <p class="indent">NYSL Fundraiser</p>
+
+        <h2>August 16</h2>
+        <p class="indent">Season Kick-off: Meet the Teams</p>
+        
+        <h2>September 1</h2>
+        <p class="indent"> First Game of the Season (Check Game Schedule for details)</p>
+      </div>
+    </div>
+  </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
