e48b5a648887c5d6aa084d3c7e699f84d1f2a063
Ignacio Gamboa <ign.gga@outlook.cl>
2020-12-30 21:47:13 -0300

    se agregan paginas restantes (contact, info, registration, rules).vue se realizan linkeos correspondientes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="nav">
     <router-link to="/home">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/contact">Contact</router-link> |
+    <router-link to="/info">Info</router-link> |
+    <router-link to="/registration">Registration</router-link> |
+    <router-link to="/rules">Rules</router-link> |
   </div>
   <router-view/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,46 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Portada from '../views/Portada.vue'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Contact from '../views/Contact.vue'
+import Info from '../views/Info.vue'
+import Registration from '../views/Registration.vue'
+import Rules from '../views/Rules.vue'
 
 const routes = [
   {
     path:'/',
     name:'Portada',
-    component: Portada
+    component:Portada
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: Home
+    path:'/home',
+    name:'Home',
+    component:Home
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path:'/about',
+    name:'About',
+    component:About
+  },
+  {
+    path:'/contact',
+    name:'Contact',
+    component:Contact
+  },
+  {
+    path:'/info',
+    name:'Info',
+    component:Info
+  },
+  {
+    path:'/registration',
+    name:'Registration',
+    component:Registration
+  },
+  {
+    path:'/rules',
+    name:'Rules',
+    component:Rules
   }
 ]
 
